f9d53b03c4264f858a760b69e22cba0123ade5cf
wawakawaka <127074263+wawakawaka@users.noreply.github.com>
2023-03-17 10:55:38 +0800

    add changes
    add changes

---
+++ Q2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int max, min;
+	cout << "Please input the maximal length allowed for the edge: ";
+	cin >> max;
+	cout << "Please input the minimal length allowed for the edge: ";
+	cin >> min;
